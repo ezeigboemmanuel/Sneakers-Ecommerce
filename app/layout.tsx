5984c6121055394d1bf0f63b96fec33b7461bf10
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-07-06 18:34:36 +0100

    update: done with navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Archivo } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const archivo = Archivo({
+  subsets: ['latin'],
+  variable: '--font-archivo',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Sneakers",
@@ -17,9 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet"></link>
+      <body className={`${inter.className}`}>
         <Navbar />
-        <div className="max-w-[1440px] mx-auto">{children}</div>
+        <div className="max-w-[1440px] mx-auto px-8">{children}</div>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,91 @@
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+import Logo from "@/assets/icons/logo.svg";
+import Link from "next/link";
+import { useState } from "react";
+import BagOutline from "@/assets/icons/bagoutline.svg";
+import BagFilled from "@/assets/icons/bagfilled.svg";
+import LoveOutline from "@/assets/icons/loveoutline.svg";
+import LoveFilled from "@/assets/icons/lovefilled.svg";
+import PersonOutline from "@/assets/icons/personoutline.svg";
+import PersonFilled from "@/assets/icons/personfilled.svg";
 
 const Navbar = () => {
-  return (
-    <div className='bg-[#1A1A1A] text-white/[0.8]'>Navbar</div>
-  )
-}
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
-export default Navbar
+  const icons = [
+    {
+      outline: BagOutline,
+      filled: BagFilled,
+    },
+    {
+      outline: LoveOutline,
+      filled: LoveFilled,
+    },
+    {
+      outline: PersonOutline,
+      filled: PersonFilled,
+    },
+  ];
+  return (
+    <div className="bg-[#1A1A1A] text-white/[0.8] flex justify-between items-center px-8 py-3">
+      <Link href="/">
+        <Image src={Logo} alt="logo" />
+      </Link>
+      <div className="flex space-x-10 ">
+        <Link href="/">
+          <p className="font-light hover:font-normal hover:text-white">Men</p>
+        </Link>
+        <Link href="/">
+          <p className="font-light hover:font-normal hover:text-white">Women</p>
+        </Link>
+        <Link href="/">
+          <p className="font-light hover:font-normal hover:text-white">Kids</p>
+        </Link>
+      </div>
+      <div className="flex space-x-8">
+        <div className="relative w-[300px] ">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-white/[0.7]"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+
+          <input
+            className="h-[36px] relative pl-10 border-[1px] border-white/[0.7] focus:border-white text-sm rounded-[8px] w-full py-2 px-3 text-white/[0.7] leading-tight focus:outline-none focus:shadow-outline bg-transparent"
+            id="links"
+            type="text"
+            placeholder="Search"
+          />
+        </div>
+        <div className="flex space-x-4 justify-center items-center">
+          {icons.map((icon, index) => (
+            <Image
+              key={index}
+              src={hoveredIndex === index ? icon.filled : icon.outline}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+              alt="icons"
+              className="w-[18.67px] h-[24px]"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
